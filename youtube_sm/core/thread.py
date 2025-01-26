3c6eacc4b6de252c133e4ab923ce7886a8f529c7
GrantAtavism <127570170+GrantAtavism@users.noreply.github.com>
2025-01-26 17:51:51 +0000

    Avoid error in 'prog' variable
    the prog variable is set to None in cases where the loading parameter is False, or when nb is neither 1 nor greater than 1. This means that when the code reaches the line if loading and prog.xmin != prog.xmax:, prog may still be None.
    
    Added a check before prog attributes accessed - the code only attempts to access the xmin and xmax attributes of prog if prog is not None

--- youtube_sm/core/thread.py
+++ youtube_sm/core/thread.py
@@ -11,47 +11,48 @@ from .tools import (
 
 
 def Run_analyze(urls, loading=False, file=None, method='0'):
-	"""Run all the analyze in a thread"""
-	threads = []
-	nb = 0
-	for site in urls:
-		nb += len(urls[site])
-	if loading and nb == 1:
-		prog = Progress_loop()
-		prog.progress_bar()
-	elif loading and nb > 1:
-		prog = Progress(nb)
-		prog.progress_bar()
-	else:
-		prog = None
-	if method == '0':
-		max_thr = 40
-	elif method == '1':
-		max_thr = 50
-	elif method == '2':
-		max_thr = 5
-	# Run threads
-	for site in urls:
-		analyzer = return_Analyzer(site)
-		if analyzer is None:
-			continue
-		for i in range(len(urls[site])):
-			thr = analyzer(urls[site][i])
-			thr.initialize(method, file, prog)
-			thr.Thread()
-			threads.append(thr)
-			log.Info("Thread start ({}{})".format(site, urls[site][i]))
-			thr.start()
-			if i > 1 and (i % max_thr == 0 or nb == i+1):
-				for y in threads:
-					y.join()
-				threads = []
-		for y in threads:
-			y.join()
-	if loading and prog.xmin != prog.xmax:
-		prog.xmin = prog.xmax
-		prog.progress_bar()
-		print()
+    """Run all the analyze in a thread"""
+    threads = []
+    nb = 0
+    for site in urls:
+        nb += len(urls[site])
+    if loading and nb == 1:
+        prog = Progress_loop()
+        prog.progress_bar()
+    elif loading and nb > 1:
+        prog = Progress(nb)
+        prog.progress_bar()
+    else:
+        prog = None
+    if method == '0':
+        max_thr = 40
+    elif method == '1':
+        max_thr = 50
+    elif method == '2':
+        max_thr = 5
+    # Run threads
+    for site in urls:
+        analyzer = return_Analyzer(site)
+        if analyzer is None:
+            continue
+        for i in range(len(urls[site])):
+            thr = analyzer(urls[site][i])
+            thr.initialize(method, file, prog)
+            thr.Thread()
+            threads.append(thr)
+            log.Info("Thread start ({}{})".format(site, urls[site][i]))
+            thr.start()
+            if i > 1 and (i % max_thr == 0 or nb == i + 1):
+                for y in threads:
+                    y.join()
+                threads = []
+        for y in threads:
+            y.join()
+    if loading and prog is not None and prog.xmin != prog.xmax:
+        prog.xmin = prog.xmax
+        prog.progress_bar()
+        print()
+
 
 
 def old(subs, days=365):
